3998071239514a3a54479d228b876c6fb3e825fe
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-09-04 08:44:19 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
