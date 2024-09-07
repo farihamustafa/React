d563aba5e67ce2ed46fdf0273f8a8757bba21bff
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-09-07 10:15:19 +0500

    .

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -5,14 +5,16 @@ function Home() {
   return (
    <>
    <img src='/poster.png' style={{ width:"100%", height:"400% "}}/>
-   <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+   <div>
+   <div class="card" style={{width:""}}>
+  <img src="..." class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+   </div>
    </>
   )
 }
