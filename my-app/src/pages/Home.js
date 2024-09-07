bc9441d0e9a0af779b05c2245fd0d6167efd925b
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-09-07 10:07:47 +0500

    .

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -3,11 +3,17 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-     
-      <h1>Home</h1>
-     
-    </div>
+   <>
+   <img src='/poster.png' style={{ width:"100%", height:"400% "}}/>
+   <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+   </>
   )
 }
 
