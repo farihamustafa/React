998749766e75b0d778416f5c41c61e9494a61fd0
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-09-10 10:49:24 +0500

    class code

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useState } from "react";
 
 
 function About() {
+  const[count, setCount] = useState(0)
+  const [countValue, setcountValue] = useState(1)
+  const [counterstatus, setcounterstatus]= useState(false);
+  
   return (
+
     <div>
+       <button className="btn btn-success mb-5" onClick={()=>setcounterstatus(!counterstatus)}>{counterstatus ? "Hid ": "Show"} Counter</button>
+      {counterstatus  &&<>
      
-      <h1>About us</h1>
+     <input className="form-control m-5" type="number" onChange={(e)=>setcountValue(parseInt(e.target.value))}/>
+      <button className='btn btn-primary m-5'onClick={()=>setCount(count+countValue)}>Count{count}</button>
+      </>}
     
     </div>
   )
