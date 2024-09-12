05c2fc26e6a431ed0b17a7ab49681261f4ed4681
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-09-12 10:08:00 +0500

    Contact form is added

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+const formstructure ={name:'',email:'',message:''}
 
 function Contact() {
+  const[data,setData]=useState(formstructure);
   return (
-    <div>
-      
-      <h1>Contact Now</h1>
+  <center>
+      <form className='mt-5' style={{width: "26rem"}} onSubmit={(e)=>{
+        e.preventDefault()
+        console.log(data)
+        setData(formstructure)
+      }}>
+    <div data-mdb-input-init class="form-outline mb-4">
+    <label class="form-label" for="form4Example1">Name</label>
+      <input value={data.name} onChange={(e)=>setData({ ...data,name:e.target.value})}  type="text" id="form4Example1" class="form-control" />
       
     </div>
+  
+    <div data-mdb-input-init class="form-outline mb-4">
+      <label class="form-label" for="form4Example2">Email address</label>
+      <input value={data.email} onChange={(e)=>setData({ ...data,email:e.target.value})}  type="text" id="form4Example1" class="form-control" />
+      
+    </div>
+  
+    <div data-mdb-input-init class="form-outline mb-4">
+      <label class="form-label" for="form4Example3">Message</label>
+      <input value={data.message} onChange={(e)=>setData({ ...data,message:e.target.value})}  type="text" id="form4Example1" class="form-control" />
+      
+    </div>
+  
+    <button type="submit" data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Send</button>
+  </form>
+  </center>
   )
 }
 
